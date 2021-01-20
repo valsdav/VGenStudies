8a6f31aecffd230420ee8eb67bac2670532c0c10
Kenneth Long <kdlong@hep.wisc.edu>
2021-01-20 20:46:40 +0100

    Add VBS WV configs

---
+++ configs/WminusToLNuWminusToJJJJ_VBS_Herwig7.py
@@ -0,0 +1,239 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/VGenStudies/python/WminusToLNuWminusToJJJJ_VBS_Herwig7.py --fileout file:WminusToLNuWminusToJJJJ_VBS_Herwig7.root --mc --eventcontent NANOAODGEN --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN --python_filename configs/WminusToLNuWminusToJJJJ_VBS_Herwig7.py --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=999 -n 30 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+
+
+process = cms.Process('NANOGEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('PhysicsTools.NanoAOD.nanogen_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(500)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/VGenStudies/python/WminusToLNuWminusToJJJJ_VBS_Herwig7.py nevts:500'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.NANOAODGENoutput = cms.OutputModule("NanoAODOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAOD'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:WminusToLNuWminusToJJJJ_VBS_Herwig7.root'),
+    outputCommands = process.NANOAODGENEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
+
+process.generator = cms.EDFilter("Herwig7GeneratorFilter",
+    configFiles = cms.vstring(),
+    crossSection = cms.untracked.double(-1),
+    dataLocation = cms.string('${HERWIGPATH:-6}'),
+    eventHandlers = cms.string('/Herwig/EventHandlers'),
+    filterEfficiency = cms.untracked.double(1.0),
+    generatorModule = cms.string('/Herwig/Generators/EventGenerator'),
+    herwig7CH3AlphaS = cms.vstring(
+        'cd /Herwig/Shower', 
+        'set AlphaQCD:AlphaIn 0.118', 
+        'cd /'
+    ),
+    herwig7CH3MPISettings = cms.vstring(
+        'set /Herwig/Hadronization/ColourReconnector:ReconnectionProbability 0.4712', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:pTmin0 3.04', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:InvRadius 1.284', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:Power 0.1362'
+    ),
+    herwig7CH3PDF = cms.vstring(
+        'cd /Herwig/Partons', 
+        'create ThePEG::LHAPDF PDFSet_nnlo ThePEGLHAPDF.so', 
+        'set PDFSet_nnlo:PDFName NNPDF31_nnlo_as_0118.LHgrid', 
+        'set PDFSet_nnlo:RemnantHandler HadronRemnants', 
+        'set /Herwig/Particles/p+:PDF PDFSet_nnlo', 
+        'set /Herwig/Particles/pbar-:PDF PDFSet_nnlo', 
+        'set /Herwig/Partons/PPExtractor:FirstPDF  PDFSet_nnlo', 
+        'set /Herwig/Partons/PPExtractor:SecondPDF PDFSet_nnlo', 
+        'set /Herwig/Shower/ShowerHandler:PDFA PDFSet_nnlo', 
+        'set /Herwig/Shower/ShowerHandler:PDFB PDFSet_nnlo', 
+        'create ThePEG::LHAPDF PDFSet_lo ThePEGLHAPDF.so', 
+        'set PDFSet_lo:PDFName NNPDF31_lo_as_0130.LHgrid', 
+        'set PDFSet_lo:RemnantHandler HadronRemnants', 
+        'set /Herwig/Shower/ShowerHandler:PDFARemnant PDFSet_lo', 
+        'set /Herwig/Shower/ShowerHandler:PDFBRemnant PDFSet_lo', 
+        'set /Herwig/Partons/MPIExtractor:FirstPDF PDFSet_lo', 
+        'set /Herwig/Partons/MPIExtractor:SecondPDF PDFSet_lo', 
+        'cd /'
+    ),
+    herwig7StableParticlesForDetector = cms.vstring(
+        'set /Herwig/Decays/DecayHandler:MaxLifeTime 10*mm', 
+        'set /Herwig/Decays/DecayHandler:LifeTimeOption Average'
+    ),
+    hw_PSWeights_settings = cms.vstring(
+        'cd /', 
+        'cd /Herwig/Shower', 
+        'do ShowerHandler:AddVariation RedHighAll 1.141 1.141  All', 
+        'do ShowerHandler:AddVariation RedLowAll 0.707 0.707 All', 
+        'do ShowerHandler:AddVariation DefHighAll 2 2 All', 
+        'do ShowerHandler:AddVariation DefLowAll 0.5 0.5 All', 
+        'do ShowerHandler:AddVariation ConHighAll 4 4 All', 
+        'do ShowerHandler:AddVariation ConLowAll 0.25 0.25 All', 
+        'do ShowerHandler:AddVariation RedHighHard 1.141 1.141  Hard', 
+        'do ShowerHandler:AddVariation RedLowHard 0.707 0.707 Hard', 
+        'do ShowerHandler:AddVariation DefHighHard 2 2 Hard', 
+        'do ShowerHandler:AddVariation DefLowHard 0.5 0.5 Hard', 
+        'do ShowerHandler:AddVariation ConHighHard 4 4 Hard', 
+        'do ShowerHandler:AddVariation ConLowHard 0.25 0.25 Hard', 
+        'do ShowerHandler:AddVariation RedHighSecondary 1.141 1.141  Secondary', 
+        'do ShowerHandler:AddVariation RedLowSecondary 0.707 0.707 Secondary', 
+        'do ShowerHandler:AddVariation DefHighSecondary 2 2 Secondary', 
+        'do ShowerHandler:AddVariation DefLowSecondary 0.5 0.5 Secondary', 
+        'do ShowerHandler:AddVariation ConHighSecondary 4 4 Secondary', 
+        'do ShowerHandler:AddVariation ConLowSecondary 0.25 0.25 Secondary', 
+        'set SplittingGenerator:Detuning 2.0', 
+        'cd /'
+    ),
+    hw_lhe_MG5aMCatNLO_settings = cms.vstring(
+        'set /Herwig/Shower/KinematicsReconstructor:InitialInitialBoostOption LongTransBoost', 
+        'set /Herwig/Shower/KinematicsReconstructor:ReconstructionOption General', 
+        'set /Herwig/Shower/KinematicsReconstructor:FinalStateReconOption Default', 
+        'set /Herwig/Shower/KinematicsReconstructor:InitialStateReconOption Rapidity', 
+        'set /Herwig/Shower/ShowerHandler:SpinCorrelations Yes', 
+        'set /Herwig/Particles/t:NominalMass 172.5'
+    ),
+    hw_lhe_common_settings = cms.vstring(
+        'read snippets/PPCollider.in', 
+        'cd /Herwig/Generators', 
+        'cd /Herwig/EventHandlers', 
+        'library LesHouches.so', 
+        'create ThePEG::LesHouchesEventHandler LesHouchesHandler', 
+        'set LesHouchesHandler:PartonExtractor /Herwig/Partons/PPExtractor', 
+        'set LesHouchesHandler:CascadeHandler /Herwig/Shower/ShowerHandler', 
+        'set LesHouchesHandler:DecayHandler /Herwig/Decays/DecayHandler', 
+        'set LesHouchesHandler:HadronizationHandler /Herwig/Hadronization/ClusterHadHandler', 
+        'set LesHouchesHandler:WeightOption VarNegWeight', 
+        'set LesHouchesHandler:Weighted On', 
+        'set /Herwig/Generators/EventGenerator:EventHandler /Herwig/EventHandlers/LesHouchesHandler', 
+        'create ThePEG::Cuts /Herwig/Cuts/NoCuts', 
+        'create ThePEG::LHAPDF /Herwig/Partons/LHAPDF ThePEGLHAPDF.so', 
+        'set /Herwig/Partons/LHAPDF:PDFName NNPDF31_nnlo_as_0118', 
+        'set /Herwig/Partons/LHAPDF:RemnantHandler /Herwig/Partons/HadronRemnants', 
+        'set /Herwig/Particles/p+:PDF /Herwig/Partons/LHAPDF', 
+        'set /Herwig/Particles/pbar-:PDF /Herwig/Partons/LHAPDF', 
+        'set /Herwig/Partons/PPExtractor:FirstPDF  /Herwig/Partons/LHAPDF', 
+        'set /Herwig/Partons/PPExtractor:SecondPDF /Herwig/Partons/LHAPDF', 
+        'create ThePEG::LesHouchesFileReader LesHouchesReader', 
+        'set LesHouchesReader:FileName cmsgrid_final.lhe', 
+        'set LesHouchesReader:AllowedToReOpen No', 
+        'set LesHouchesReader:InitPDFs 0', 
+        'set LesHouchesReader:Cuts /Herwig/Cuts/NoCuts', 
+        'set LesHouchesReader:MomentumTreatment RescaleEnergy', 
+        'set LesHouchesReader:PDFA /Herwig/Partons/LHAPDF', 
+        'set LesHouchesReader:PDFB /Herwig/Partons/LHAPDF', 
+        'insert LesHouchesHandler:LesHouchesReaders 0 LesHouchesReader'
+    ),
+    hw_user_settings = cms.vstring(
+        'cd /Herwig/EventHandlers', 
+        'set EventHandler:LuminosityFunction:Energy 13000*GeV', 
+        'cd /'
+    ),
+    parameterSets = cms.vstring(
+        'hw_lhe_common_settings', 
+        'hw_lhe_MG5aMCatNLO_settings', 
+        'herwig7CH3PDF', 
+        'herwig7CH3AlphaS', 
+        'herwig7CH3MPISettings', 
+        'herwig7StableParticlesForDetector', 
+        'hw_PSWeights_settings', 
+        'hw_user_settings'
+    ),
+    repository = cms.string('${HERWIGPATH}/HerwigDefaults.rpo'),
+    run = cms.string('InterfaceMatchboxTest'),
+    runModeList = cms.untracked.string('read,run')
+)
+
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/madgraph/V5_2.4.2/VVjj_SemiLeptonic/WMlepWMhadJJ_EWK_LO_SM_mjj100_pTj10_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
+    generateConcurrently = cms.untracked.bool(False),
+    nEvents = cms.untracked.uint32(500),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.generation_step = cms.Path(process.pgen)
+process.nanoAOD_step = cms.Path(process.nanogenSequence)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODGENoutput_step = cms.EndPath(process.NANOAODGENoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.nanoAOD_step,process.endjob_step,process.NANOAODGENoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	if path in ['lhe_step']: continue
+	getattr(process,path).insert(0, process.ProductionFilterSequence)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.NanoAOD.nanogen_cff
+from PhysicsTools.NanoAOD.nanogen_cff import customizeNanoGEN 
+
+#call to customisation function customizeNanoGEN imported from PhysicsTools.NanoAOD.nanogen_cff
+process = customizeNanoGEN(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=999
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
